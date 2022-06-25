7d702415008192fad177a12c113f954372793c16
Eduardo Cruz <eduardo.cruz@insti.com.br>
2022-06-25 12:41:19 -0300

    first commit

---
+++ src/main/java/br/dev/ec/unidata/resources/domain/usuario/Role.java
@@ -0,0 +1,19 @@
+package br.dev.ec.unidata.resources.domain.usuario;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+@Table(schema = "unidata", name = "roles")
+public class Role {
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private long id;
+	private String name;
+}
